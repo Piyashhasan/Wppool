13313ffe100346be9ce960613cbb8fc81b0f6325
Piyashhasan <piyashhasan38@gmail.com>
2024-07-11 16:17:15 +0600

    feat: Carousel make - done

--- src/components/CarouselItems/CarouselItems.jsx
+++ src/components/CarouselItems/CarouselItems.jsx
@@ -1,12 +1,13 @@
-// "use client";
+"use client";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { carouselItems } from "@/utils/carouselItems";
 import Image from "next/image";
+import { useRef, useState } from "react";
+import { Dropdown } from "primereact/dropdown";
 
 const CarouselItems = () => {
-  carouselItems;
   const settings = {
     dots: true,
     infinite: false,
@@ -41,9 +42,25 @@ const CarouselItems = () => {
       },
     ],
   };
+
+  const [searchCarousel, setSearchCarousel] = useState(null);
+  const searchInput = [
+    { slideNo: "1 and 2" },
+    { slideNo: "2 and 3" },
+    { slideNo: "3 and 4" },
+    { slideNo: "4 and 5" },
+  ];
+  const sliderRef = useRef(null);
+  const handleDropdownChange = (e) => {
+    setSearchCarousel(e.value);
+    const value = e.value.slideNo;
+    const [start, end] = value.split(" and ").map(Number);
+    sliderRef.current.slickGoTo(start - 1);
+  };
+
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
+    <div className="slider-container relative pb-[150px]">
+      <Slider ref={sliderRef} {...settings}>
         {carouselItems.map((item, index) => {
           return (
             <div
@@ -59,6 +76,27 @@ const CarouselItems = () => {
           );
         })}
       </Slider>
+      <div className="absolute bottom-[88px] left-[150px]">
+        <div className="flex items-center gap-[10px]">
+          <small>Pages</small>
+          <div className="drop-down-btn">
+            <Dropdown
+              value={searchCarousel}
+              onChange={handleDropdownChange}
+              options={searchInput}
+              optionLabel="slideNo"
+              placeholder="1 and 2"
+              className="w-[130px] rounded-full text-white"
+            />
+          </div>
+          <small>of 37</small>
+        </div>
+      </div>
+      <div className="absolute right-[70px] mt-[15px]">
+        <button className="border-none outline-none py-[23px] px-[20px] rounded-full bg-[#2042B6] text-white cursor-pointer">
+          Download the 2023 Report
+        </button>
+      </div>
     </div>
   );
 };
